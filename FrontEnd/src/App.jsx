8abbc8ba9416800f10c97c2e74de23a2e38f6450
Zaki-Mohd <zakilol21427@gmail.com>
2025-06-21 12:11:49 +0530

    Added Footer styling

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -41,7 +41,7 @@ int main() {
 
   return (
     <>
-    <div className="page-wrapper">
+    
       <header className='head'> Z++ Code Editor </header>
       <main>
         <div className="left">
@@ -86,7 +86,7 @@ int main() {
       </a>  */}
 
       </footer>
-      </div>
+    
     </>
   )
 }
